ac22b0837b83e48d4182e467fffdb5811170599f
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2025-05-31 11:34:25 +0200

    create new profile if we create new user

---
+++ accounts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from utils.generate_code import generate_code
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,related_name='profile_user',on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='images_user')
+    code=models.CharField(max_length=75,default=generate_code)
+    phone=models.CharField(max_length=75)
+
+    def __str__(self):
+        return str(self.user)
+    
+
+
+@receiver(post_save,sender=User)   
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
+    
